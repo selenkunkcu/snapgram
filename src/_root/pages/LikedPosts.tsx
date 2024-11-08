cca8439b245d175668df3ee5ce2030dca21e3da8
selenkunkcu <selenkunkcu@gmail.com>
2024-11-09 00:30:54 +0300

    Liked posts added

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,5 +1,25 @@
+import { useGetCurrentUser } from "../../lib/react-query/queriesAndMutations";
+
+import Loader from "../../components/shared/Loader";
+import GridPostList from "../../components/shared/GridPostList";
+
 const LikedPosts = () => {
-  return <div>Coming Soon: This feature will be available soon.</div>;
+  const { data: currentUser } = useGetCurrentUser();
+
+  if (!currentUser)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
+  return (
+    <>
+      {currentUser?.liked?.length === 0 && <p className="text-light-4"> No liked posts</p>}
+
+      <GridPostList posts={currentUser?.liked} showStats={false} />
+    </>
+  );
 };
 
 export default LikedPosts;
